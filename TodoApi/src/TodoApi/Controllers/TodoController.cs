16c4b2f5fbbdc449b85fedcc180bef0f5c0c8373
lyy <l_yy@live.com>
2016-01-04 21:24:00 +0800

    。

---
+++ TodoApi/src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using TodoApi.Models;
+
+namespace SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        [FromServices]
+        public ITodoRepository TodoItems { get; set; }
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+    }
+}
